5130cc18f015d47983b172a4b00848201de0dc65
Fersho Pls <shaaf4ever@gmail.com>
2019-02-19 18:39:51 -0600

    not working OOP server

--- server.js
+++ server.js
@@ -4,18 +4,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-// var mongoose = require('mongoose');
-
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
-
-// var Message = mongoose.model('Message',{
-//   name : String,
-//   message : String
-// })
-
-// var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
 
 function Player (settings)
 {
@@ -159,10 +150,275 @@ io.on('connection', (socket) => {
 });
 
 
-// mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
-//   console.log('mongodb connected',err);
-// })
-
 var server = http.listen(3000, () => {
-  console.log('server is running on port', server.address().port);
+	console.log('server is running on port', server.address().port);
 });
+
+
+
+var StackMaster = {
+	stack: [],
+	current_dimension: null,
+	name: 'none',
+
+	dimension: function(id, callback)
+	{
+		if (typeof id == 'undefined')
+		{
+			this.current_dimension = null
+		}
+		else if (typeof callback == 'undefined')
+		{
+			this.current_dimension = id
+		}
+		else
+		{
+			callback.call(this, this.name)
+			this.dimension()
+		}
+	},
+
+	getDimension: function()
+	{
+		if (!this.current_dimension)
+			return this.stack
+		
+		if (!this.stack.hasOwnProperty(this.current_dimension))
+			this.stack[this.current_dimension] = {}
+
+		return this.stack[this.current_dimension]
+	},
+
+	get: function(id, dimension)
+	{
+		if (typeof id == 'undefined')
+			return this.getDimension()
+		
+		if (typeof dimension == 'undefined')
+		{
+			if (this.getDimension().hasOwnProperty(id))
+				return this.getDimension()[id]
+		} else {
+			this.dimension(dimension)
+			var value = this.get(id)
+			this.dimension()
+			return value
+		}
+		return null
+	},
+
+	has: function(id)
+	{
+		return this.get().hasOwnProperty(id)
+	},
+
+	removeById: function (id)
+	{
+		if (this.has(id))
+			delete this.get()[id]
+	},
+
+	removeByIndex: function (id)
+	{
+		if (this.has(id))
+			this.get().splice(id, 1)
+	},
+
+	push: function(item)
+	{
+		this.get().push(item)
+	},
+
+	add: function(id, item)
+	{
+		this.get()[id] = item
+	},
+
+	set: function(id, value)
+	{
+		console.log('STACK SET',id,value)
+		this.get()[id] = value
+	},
+
+	addMany: function(items)
+	{
+		for (id in items)
+		{
+			var item = items[id]
+			this.set(id, item)
+		}
+	},
+
+	for: function(callback)
+	{
+		var items = this.get()
+		this.loop(items, callback)
+	},
+
+	loop: function(items, callback, context)
+	{
+		context = context?context:callback
+		for (id in items)
+		{
+			if (!items.hasOwnProperty(id))
+				continue
+
+			callback.call(context, id, items[id], items)
+		}
+	}
+}
+
+/*
+player.stack.get()
+player.stack.removeById()
+*/
+// Controladores
+//	-> acceso y edición a modulo
+// Stack
+//	-> 
+// Modulos
+//	-> propiedades
+//	-> propiedades sincronizables
+//		-> detectar cambios
+//		-> un sentido / ambos sentidos
+//			-> allow_server_update / allow_client_update
+
+def = (function(v, d) { return typeof v=='undefined'?d:v })
+function Property (id, default_value, allow_sync, allow_sync_client_update)
+{
+
+	this.id = id
+	this.default_value = def(default_value, null)
+	this.allow_sync = def(allow_sync, true)
+	this.allow_sync_client_update = def(allow_sync_client_update, false)
+}
+
+var ModuleMaster = {
+	properties: {},
+	values: [],
+
+	init: function(propertiesList){
+		this.values = Object.create(StackMaster)
+		this.values.name = 'Module Master Values Stack'
+
+		StackMaster.loop(propertiesList, function(i, property){
+			this.registerProperty(property)
+		}, this)
+		return this
+	},
+
+	registerProperty: function(property)
+	{
+		if (property.hasOwnProperty('id')
+			&& property instanceof Property)
+			this.properties[property.id] = property
+	},
+
+	getProperty: function(id)
+	{
+		if (!id)
+			return this.properties
+		if (this.properties.hasOwnProperty(id))
+			return this.properties[id]
+		return null
+	},
+
+	getKeysWithValues: function()
+	{
+		var keys = this.getProperty()
+		var dic = {}
+
+		StackMaster.loop(keys, function(id, property){
+			dic[id] = this.get(id, property.default_value)
+		}, this)
+		
+		return dic
+	},
+
+	hasProperty: function(id)
+	{
+		return this.properties.indexOf(id) != -1
+	},
+
+	get: function(value_id, id, default_value)
+	{
+		if (typeof value_id == 'undefined')
+		{
+			return this.values.get()
+		}
+		
+		if (this.values.has(value_id))
+		{
+			if (typeof id == 'undefined')
+			{
+				return this.values.get(value_id)
+			} else {
+				return def(this.values.get(id, value_id), def(default_value, null))
+			}
+		}
+		return null
+	},
+
+	setSingleProperty: function (value_id, key, value)
+	{
+		var property = this.getProperty(key)
+		if (property)
+		{
+			this.values.dimension(value_id, function (){
+				this.set(key, value)
+			})
+			if (property.allow_sync)
+				this.syncProperty(property.id)
+		}
+	},
+
+	set: function(value_id, dicKeyValue)
+	{
+		console.log('MODULE attempt to set', 'hello')
+		this.values.dimension(value_id, function(a){
+			this.set('hello', 1)
+		})
+	},
+
+	syncProperty: function (id)
+	{
+		console.log('property',id,'change',this.get(id))
+	},
+}
+
+var StackModuleMaster = Object.create(StackMaster)
+StackModuleMaster = Object.assign(StackModuleMaster, {
+	name: 'Stack Module Master',
+
+	create: function(id, properties) {
+		this.add(id, this.initModule(properties))
+	},
+
+	initModule: function (properties)
+	{
+		var module = Object.assign({}, ModuleMaster)
+		return module.init(properties)
+	}
+})
+
+StackModuleMaster.create('players_module', [
+		new Property('X', 0),
+		new Property('Y', 0),
+		new Property('Z', 0),
+	])
+var players = StackModuleMaster.get('players_module')
+
+/*
+StackModuleMaster [stack]
+	-> PlayersModule [module]
+		-> values_ids [stack]
+			-> properties
+*/
+
+players.set('X0001', {
+	X: 998,
+	Y: 666,
+	Z: 0,
+})
+
+console.log('MODULES:', Object.keys(StackModuleMaster.get()))
